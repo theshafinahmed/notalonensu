395d17e2163cd1f05022166547050adf2445f42b
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 10:18:53 +0600

    Just a little bit changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 function Landing() {
-    return <main className="flex items-center justify-center">In the name of Allah!</main>;
+    return (
+        <main className="flex items-center justify-center w-rscreen h-rscreen">
+            <h1 className="text-2xl">In the name of Allah!</h1>
+        </main>
+    );
 }
 
 export default Landing;
