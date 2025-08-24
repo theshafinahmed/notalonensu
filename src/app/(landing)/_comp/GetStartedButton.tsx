8d59a50ce3ab3462f468dc1ad90eb07376b0466f
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 14:51:59 +0600

    Separate Get Started button created

---
+++ src/app/(landing)/_comp/GetStartedButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+function GetStartedButton() {
+    const router = useRouter();
+
+    const handleGetStarted = () => {
+        router.push("/home");
+    };
+
+    return (
+        <div className="my-4">
+            <button
+                onClick={handleGetStarted}
+                className="btn btn-primary btn-lg rounded-md duration-100"
+            >
+                Get Started
+            </button>
+        </div>
+    );
+}
+
+export default GetStartedButton;
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,28 +1,13 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import GetStartedButton from "./_comp/GetStartedButton";
 
 function Landing() {
-    const router = useRouter();
-
-    const handleGetStarted = () => {
-        router.push("/home");
-    };
-
     return (
         <main className="flex flex-col gap-2 items-center justify-center w-rscreen h-rscreen">
             <h1 className="text-4xl font-bold text-primary">NotAloneNSU</h1>
             <p className="text-2xl text-center">
                 A platform for NSU students to connect with each other
             </p>
-            <div className="my-4">
-                <button
-                    onClick={handleGetStarted}
-                    className="btn btn-primary btn-lg rounded-md duration-100"
-                >
-                    Get Started
-                </button>
-            </div>
+            <GetStartedButton />
         </main>
     );
 }
