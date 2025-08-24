796f128e8463c596c3bbc760ce8a901a771674f5
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 14:42:30 +0600

    Landing page design corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 function Landing() {
     return (
         <main className="flex flex-col gap-2 items-center justify-center w-rscreen h-rscreen">
-            <h1 className="text-4xl font-bold">NotAloneNSU</h1>
-            <p className="text-2xl">
+            <h1 className="text-4xl font-bold text-primary">NotAloneNSU</h1>
+            <p className="text-2xl text-center">
                 A platform for NSU students to connect with each other
             </p>
             <div className="my-4">
-                <button className="btn btn-primary btn-lg rounded-md">
+                <button className="btn btn-primary btn-lg rounded-md duration-100">
                     Get Started
                 </button>
             </div>
