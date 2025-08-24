c8e3ba65b2e35372b91a06a2bcc926440c818a70
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 14:45:48 +0600

    Home page created and connected with Get Started button in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 function Landing() {
+    const router = useRouter();
+
+    const handleGetStarted = () => {
+        router.push("/home");
+    };
+
     return (
         <main className="flex flex-col gap-2 items-center justify-center w-rscreen h-rscreen">
             <h1 className="text-4xl font-bold text-primary">NotAloneNSU</h1>
@@ -6,7 +16,10 @@ function Landing() {
                 A platform for NSU students to connect with each other
             </p>
             <div className="my-4">
-                <button className="btn btn-primary btn-lg rounded-md duration-100">
+                <button
+                    onClick={handleGetStarted}
+                    className="btn btn-primary btn-lg rounded-md duration-100"
+                >
                     Get Started
                 </button>
             </div>
