6dc7f9f2120a0595cdda8f317105d31ce019064d
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 14:49:51 +0600

    Code organized

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <main>Home</main>;
+    return (
+        <main className="w-rscreen h-rscreen flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Home Screen</h1>
+        </main>
+    );
 }
 
 export default Home;
