41189b2a10c1fd3873de1938d8515ae4763bf114
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-24 14:34:52 +0600

    User select disabled and basic landing page designed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 function Landing() {
     return (
-        <main className="flex items-center justify-center w-rscreen h-rscreen">
-            <h1 className="text-2xl">In the name of Allah!</h1>
+        <main className="flex flex-col gap-2 items-center justify-center w-rscreen h-rscreen">
+            <h1 className="text-4xl font-bold">NotAloneNSU</h1>
+            <p className="text-2xl">
+                A platform for NSU students to connect with each other
+            </p>
+            <div className="my-4">
+                <button className="btn btn-primary btn-lg rounded-md">
+                    Get Started
+                </button>
+            </div>
         </main>
     );
 }
