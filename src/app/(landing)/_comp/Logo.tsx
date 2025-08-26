bcfe62e8fda387d5e965305e8f089dc74aca1d8f
Shafin Ahmed <theshafinahemd2003@gmail.com>
2025-08-26 08:30:51 +0600

    Logo design updated

---
+++ src/app/(landing)/_comp/Logo.tsx
@@ -0,0 +1,11 @@
+function Logo() {
+    return (
+        <h1 className="text-4xl font-bold">
+            <span className="text-secondary">Not</span>
+            <span className="text-accent">Alone</span>
+            <span className="text-primary">NSU</span>
+        </h1>
+    );
+}
+
+export default Logo;
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,12 @@
 import GetStartedButton from "./_comp/GetStartedButton";
+import Logo from "./_comp/Logo";
 
 function Landing() {
     return (
         <main className="flex flex-col gap-2 items-center justify-center w-rscreen h-rscreen">
-            <h1 className="text-4xl font-bold text-primary">NotAloneNSU</h1>
-            <p className="text-2xl text-center">
-                A platform for NSU students to connect with each other
+            <Logo />
+            <p className="text-xl text-center">
+                Feeling alone at NSU? Not anymore!
             </p>
             <GetStartedButton />
         </main>
